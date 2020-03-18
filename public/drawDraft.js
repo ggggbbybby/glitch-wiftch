187720639f6cef6a11d03cbdae43433d1962637e
Glitch (hello-express) <none>
2020-03-18 02:29:20 +0000

    👾🍀 Checkpoint ./public/generateWIF.js:660488/3328 ./public/script.js:660488/24078 ./public/drawDraft.js:660488/1509 ./views/index.html:660488/60

---
+++ public/drawDraft.js
@@ -0,0 +1,51 @@
+const drawDraft = function(svg, draft) {
+
+  const fill_drawdown = function(i, j) {
+    let col = i % draft.warp.length;
+    let row = j % draft.weft.length;
+    let treadle = draft.weft[row];
+    let shafts_down = draft.tieup[treadle - 1];
+    if (!shafts_down) debugger;
+    let shaft = draft.warp[col];
+    return shafts_down.includes(shaft);
+  };
+
+  const fill_threading = function(col, row) {
+    return draft.warp[col % draft.warp.length] == row + 1;
+  };
+
+  const fill_tieup = function(i, j) {
+    let treadle = i % draft.treadle_count;
+    let shaft = (j % draft.shaft_count) + 1;
+    if (!draft.tieup[treadle]) debugger;
+    return draft.tieup[treadle].includes(shaft);
+  };
+
+  const fill_treadling = function(i, j) {
+    let treadle = (i % draft.treadle_count) + 1;
+    let row = j % draft.weft.length;
+    return draft.weft[row] == treadle;
+  };
+  
+  svg.querySelectorAll('rect').forEach((child) => {
+    let fill = false;
+    const col = parseInt(child.dataset.thread);
+    const row = parseInt(child.dataset.treadle);
+    switch (child.dataset.type) {
+      case 'drawdown':
+        fill = fill_drawdown(col, row) ? "#f00" : "#fff";
+        break;
+      case 'warp':
+        fill = fill_threading(col, row) ? "#000" : "#fff";
+        break;
+      case 'weft':
+        fill = fill_treadling(col, row) ? "#000" : "#fff";
+        break;
+      case 'tieup':
+        fill = fill_tieup(col, row) ? "#000" : "#fff";
+        break;
+    }
+    child.setAttribute('fill', fill);
+    
+  });
+};
--- public/script.js
+++ public/script.js
@@ -21,145 +21,7 @@ const tieupClick = function(shaft, treadle, draft) {
   drawDraft(draft);
 }
 
-const drawDraft = function(draft) {
 
-  const fill_drawdown = function(i, j) {
-    let col = i % draft.warp.length;
-    let row = j % draft.weft.length;
-    let treadle = draft.weft[row];
-    let shafts_down = draft.tieup[treadle - 1];
-    if (!shafts_down) debugger;
-    let shaft = draft.warp[col];
-    return shafts_down.includes(shaft);
-  };
-
-  const fill_threading = function(col, row) {
-    return draft.warp[col % draft.warp.length] == row + 1;
-  };
-
-  const fill_tieup = function(i, j) {
-    let treadle = i % draft.treadle_count;
-    let shaft = (j % draft.shaft_count) + 1;
-    if (!draft.tieup[treadle]) debugger;
-    return draft.tieup[treadle].includes(shaft);
-  };
-
-  const fill_treadling = function(i, j) {
-    let treadle = (i % draft.treadle_count) + 1;
-    let row = j % draft.weft.length;
-    return draft.weft[row] == treadle;
-  };
-  
-  drawdown.querySelectorAll('rect').forEach((child) => {
-    let fill = false;
-    const col = parseInt(child.dataset.thread);
-    const row = parseInt(child.dataset.treadle);
-    switch (child.dataset.type) {
-      case 'drawdown':
-        fill = fill_drawdown(col, row) ? "#f00" : "#fff";
-        break;
-      case 'warp':
-        fill = fill_threading(col, row) ? "#000" : "#fff";
-        break;
-      case 'weft':
-        fill = fill_treadling(col, row) ? "#000" : "#fff";
-        break;
-      case 'tieup':
-        fill = fill_tieup(col, row) ? "#000" : "#fff";
-        break;
-    }
-    child.setAttribute('fill', fill);
-    
-  });
-
-  const threading_sequence = function(draft) {
-    return draft.warp.map((shaft, index) => `${index + 1}=${shaft}`);
-  };
-
-  const tieup_sequence = function(draft) {
-    return draft.tieup.map(
-      (shafts, index) => `${index + 1}=${shafts.join(",")}`
-    );
-  };
-
-  const treadling_sequence = function(draft) {
-    return draft.weft.map((treadle, index) => `${index + 1}=${treadle}`);
-  };
-
-  const today = new Date().toDateString();
-  const title = document.getElementsByTagName("h1")[0].textContent;
-  let wif = [
-    "",
-    "[WIF]",
-    "[Version=1.1]",
-    `Date=${today}`,
-    "Developers=ggggbbybby@gmail.com",
-    "Source Program=glitch-wiftch",
-    "Source Version=1",
-    "",
-    "[CONTENTS]",
-    "COLOR PALETTE=true",
-    "TEXT=true",
-    "WEAVING=true",
-    "WARP=true",
-    "WEFT=true",
-    "COLOR TABLE=true",
-    "THREADING=true",
-    "TIEUP=true",
-    "TREADLING=true",
-    "",
-    "[TEXT]",
-    `Title=${title}`,
-    "",
-    "[THREADING]",
-    ...threading_sequence(draft),
-    "",
-    "[TIEUP]",
-    ...tieup_sequence(draft),
-    "",
-    "[TREADLING]",
-    ...treadling_sequence(draft),
-    "",
-    "[WEAVING]",
-    "Rising Shed=true",
-    "Shafts=4",
-    "Treadles=6",
-    "",
-    "[WARP]",
-    "Units=centimeters",
-    "Color=1",
-    `Threads=${draft.warp.length}`,
-    "Spacing=0.2117",
-    "Thickness=0.2117",
-    "",
-    "[WEFT]",
-    "Units=centimeters",
-    "Color=2",
-    `Threads=${draft.weft.length}`,
-    "Spacing=0.2117",
-    "Thickness=0.2117",
-    "",
-    "[COLOR TABLE]",
-    "1=255,255,255",
-    "2=255,0,0",
-    "",
-    "[COLOR PALETTE]",
-    "Entries=2",
-    "Range=0,255"
-  ];
-
-  const wif_out = wif.join("\n");
-  // console.log(wif_out);
-
-  const wifbox = document.getElementById("wif-preview");
-  wifbox.textContent = wif_out;
-
-  const download = document.getElementById("download-wif");
-  const wif_file = new Blob([wif_out], { type: "text/plain" });
-  const url = window.URL.createObjectURL(wif_file);
-  download.href = url;
-  download.download = `${title}.wif`;
-};
 
 
 
@@ -172,8 +34,6 @@ let draft = {
 };
 
 
-drawGrid(drawdown, {warps: 65, wefts: 65, shafts: 4, treadles: 6});
-drawDraft(draft);
 const pt = drawdown.createSVGPoint();
 const toggle_shaft = (shafts, shaft) => {
   if (shafts.includes(shaft)) {
@@ -182,6 +42,7 @@ const toggle_shaft = (shafts, shaft) => {
     return [shaft, ...shafts];
   }
 }
+
 drawdown.addEventListener('click', (click) => {
   pt.x = click.clientX;
   pt.y = click.clientY;
@@ -218,5 +79,9 @@ drawdown.addEventListener('click', (click) => {
   if (col_type == 'warp' && row_type == 'shaft') draft.warp[col] = row + 1
   if (col_type == 'treadle' && row_type == 'weft') draft.weft[row] = col + 1;
   if (col_type == 'treadle' && row_type == 'shaft') draft.tieup[col] = toggle_shaft(draft.tieup[col], row + 1);
-  drawDraft(draft);
+  drawDraft(drawdown, draft);
+  generateWIF(draft);
 })
+
+drawGrid(drawdown, {warps: 65, wefts: 65, shafts: 4, treadles: 6});
+drawDraft(drawdown, draft);
