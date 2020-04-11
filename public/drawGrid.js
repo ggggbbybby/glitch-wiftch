04b7ec6d0e879c25f5bd3dfcc5001c3b3d28be61
Glitch (hello-express) <none>
2020-04-11 19:04:55 +0000

    🗃⏰ Checkpoint ./public/drawGrid.js:660488/1021

--- public/drawGrid.js
+++ public/drawGrid.js
@@ -1,8 +1,8 @@
 
-const box = function(x, y, data={}) {
+const box = function(x, y, pixel_size, data={}) {
   let box = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  box.setAttribute("height", pixel_width);
-  box.setAttribute("width", pixel_width);
+  box.setAttribute("height", pixel_size);
+  box.setAttribute("width", pixel_size);
   box.style.stroke = "#333";
   box.style.strokeWidth = "2px";
   box.setAttribute("x", x);
@@ -14,39 +14,39 @@ const box = function(x, y, data={}) {
   return box;
 }
 
-const threadbox = function(thread, treadle) {
+const threadbox = function(thread, treadle, pixel_size) {
   // threads start at (800, 800) & goes left-up
   const type = 'drawdown'
-  const x = drawdown_width - (thread + 1)*pixel_width;
-  const y = drawdown_width - (treadle + 1)*pixel_width;
-  return box(x, y, {thread, treadle, type});
+  const x = drawdown_width - (thread + 1)*pixel_size;
+  const y = drawdown_width - (treadle + 1)*pixel_size;
+  return box(x, y, pixel_size, {thread, treadle, type});
 }
 
-const warpbox = function(thread, treadle) {
+const warpbox = function(thread, treadle, pixel_width) {
   // warps start at (800, 820) & goes left-down
   const type = 'warp'
-  const x = drawdown_width - (thread + 1)*pixel_width;
-  const y = drawdown_width + (treadle + 1)*pixel_width;
-  return box(x, y, {thread, treadle, type});
+  const x = drawdown_width - (thread + 1)*pixel_size;
+  const y = drawdown_width + (treadle + 1)*pixel_size;
+  return box(x, y, pixel_size, {thread, treadle, type});
 }
 
-const weftbox = function(thread, treadle) {
+const weftbox = function(thread, treadle, pixel_size) {
   // wefts start at (820, 800) & goes right-up
   const type = 'weft'
-  const x = drawdown_width + (thread + 1)*pixel_width;
-  const y = drawdown_width - (treadle + 1)*pixel_width;
-  return box(x, y, {thread, treadle, type});
+  const x = drawdown_width + (thread + 1)*pixel_size;
+  const y = drawdown_width - (treadle + 1)*pixel_size;
+  return box(x, y, pixel_size, {thread, treadle, type});
 }
 
-const tieupbox = function(thread, treadle) {
+const tieupbox = function(thread, treadle, pixel_size) {
   const type = 'tieup'
-  const x = drawdown_width + (thread + 1)*pixel_width;
-  const y = drawdown_width + (treadle + 1)*pixel_width;
-  return box(x, y, {thread, treadle, type});
+  const x = drawdown_width + (thread + 1)*pixel_size;
+  const y = drawdown_width + (treadle + 1)*pixel_size;
+  return box(x, y, pixel_size, {thread, treadle, type});
 }
 
 const drawGrid = function(svg, options) {
-  const {warps, wefts, shafts, treadles} = options;
+  const {pixel_size, warps, wefts, shafts, treadles} = options;
   for (let i = 0; i < warps + treadles + 1; i++) {
     for (let j = 0; j < wefts + shafts + 1; j++) {
       // when i == width or j == height, make a gap for readability
@@ -55,10 +55,10 @@ const drawGrid = function(svg, options) {
       
       let box = null;
       if (col_type == 'gap' || row_type == 'gap') box = null;
-      else if (col_type == 'thread' && row_type == 'thread') box = threadbox(i, j);
-      else if (col_type == 'thread' && row_type == 'shaft') box = warpbox(i, j%wefts - 1);
-      else if (col_type == 'treadle' && row_type == 'thread') box = weftbox(i%warps - 1 , j);
-      else if (col_type == 'treadle' && row_type == 'shaft') box = tieupbox(i%warps - 1, j%wefts - 1);
+      else if (col_type == 'thread' && row_type == 'thread') box = threadbox(i, j, pixel_size);
+      else if (col_type == 'thread' && row_type == 'shaft') box = warpbox(i, j%wefts - 1, pixel_size);
+      else if (col_type == 'treadle' && row_type == 'thread') box = weftbox(i%warps - 1 , j, pixel_size);
+      else if (col_type == 'treadle' && row_type == 'shaft') box = tieupbox(i%warps - 1, j%wefts - 1, pixel_size);
       else box = null;
       
       if (box) svg.appendChild(box);
