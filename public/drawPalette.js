aeff7771e98472107aa4117d7513e84c30368f26
Glitch (hello-express) <none>
2020-04-25 08:59:06 +0000

    🚨💧 Checkpoint ./public/drawPalette.js:660488/79

--- public/drawPalette.js
+++ public/drawPalette.js
@@ -1,18 +1,19 @@
 const swatch_size = 50;
 
-const swatch = function(fill) {
+const swatch = function(fill, idx) {
   let box = document.createElementNS("http://www.w3.org/2000/svg", "rect");
   box.setAttribute("height", swatch_size);
   box.setAttribute("width", swatch_size);
   box.style.stroke = "#333";
   box.style.strokeWidth = "2px";
   box.setAttribute("fill", fill);
+  box.setAttribute("y", swatch_size * idx);
   return box;
 }
 
 const drawPalette = function(svg) {
   const colors = ['red', 'yellow', 'blue'];
-  colors.forEach((color) => {
-    svg.appendChild(swatch(color));
+  colors.forEach((color, idx) => {
+    svg.appendChild(swatch(color, idx));
   })
 }
